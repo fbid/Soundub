b6c3ba608da729250de481abcabb0235c535c358
fbid <federico.bidoggia@gmail.com>
2017-05-23 22:22:14 +0200

    settings dictionary reformat

--- audioProcessing.py
+++ audioProcessing.py
@@ -6,7 +6,6 @@ from setup import out_path
 from setup import settings
 from operator import add
 
-
 def audioProcessing(sample_list):
     procs = []
     for s in sample_list:
@@ -16,13 +15,13 @@ def audioProcessing(sample_list):
         if settings['SILENCE']:
             sample = strip_silence(sample, 1000, silence_thresh=(sample.dBFS-6))
         if settings['PADDING']:
-            sample += AudioSegment.silent(duration=PADDING)
+            sample += AudioSegment.silent(duration=settings['PADDING'])
         if settings['PB_SPEED'] != 1:
-            sample = speedup(sample, PB_SPEED)
+            sample = speedup(sample, settings['PB_SPEED'])
         if not settings['CONCAT']:
             # Exporting indivudial samples
             sample_name = s.split('/')[-1].split('.')[0]
-            export(sample, sample_name)
+            export(sample, sample_name, settings['OUTPUT_FORMAT'], settings['OUTPUT_BITRATE'])
         else:
             procs.append(sample)
 
@@ -35,10 +34,10 @@ def audioProcessing(sample_list):
 
 def concatSamples(sample_list, output_chain_name):
     playlist = reduce(add,(s for s in sample_list))
-    export(playlist, output_chain_name)
+    export(playlist, output_chain_name, settings['OUTPUT_FORMAT'], settings['OUTPUT_BITRATE'])
 
-def export(sample, sample_name):
-    if OUTPUT_FORMAT == 'mp3':
-        sample.export(join(out_path,sample_name+'.'+OUTPUT_FORMAT),format=OUTPUT_FORMAT,bitrate=OUTPUT_BITRATE)
+def export(sample, sample_name, frmt, bitrate):
+    if frmt == 'mp3':
+        sample.export(join(out_path,sample_name+'.'+frmt),format=frmt,bitrate=bitrate)
     else:
-        sample.export(join(out_path,sample_name+'.'+OUTPUT_FORMAT),format=OUTPUT_FORMAT)
+        sample.export(join(out_path,sample_name+'.'+frmt),format=frmt)
--- setup.py
+++ setup.py
@@ -8,11 +8,13 @@ cwd = dirname(abspath(__file__))
 src_path = join(cwd,cp.get('Config', 'source_dir'))
 out_path = join(cwd,cp.get('Config', 'output_dir'))
 
-settings['NORMALIZE'] = cp.get('Config', 'normalize') == 'on'
-settings['SILENCE'] = cp.get('Config', 'cut_silence') == 'on'
-settings['REM_INDIVIDUAL_SAMPLES'] = cp.get('Config', 'remove_individual_samples') == 'on'
-settings['CONCAT'] = _cp.get('Config', 'sample_chain') == 'on'
-settings['PADDING'] = float(cp.get('Config', 'sample_padding')) * 1000
-settings['PB_SPEED'] = float(cp.get('Config','playback_speed'))
-settings['OUTPUT_FORMAT'] = cp.get('Config','output_format')
-settings['OUTPUT_BITRATE'] = cp.get('Config','output_bitrate')
+settings = {
+    'NORMALIZE': cp.get('Config', 'normalize') == 'on',
+    'SILENCE': cp.get('Config', 'cut_silence') == 'on',
+    'REM_INDIVIDUAL_SAMPLES': cp.get('Config', 'remove_individual_samples') == 'on',
+    'CONCAT': cp.get('Config', 'sample_chain') == 'on',
+    'PADDING': float(cp.get('Config', 'sample_padding')) * 1000,
+    'PB_SPEED': float(cp.get('Config','playback_speed')),
+    'OUTPUT_FORMAT': cp.get('Config','output_format'),
+    'OUTPUT_BITRATE': cp.get('Config','output_bitrate')
+}
